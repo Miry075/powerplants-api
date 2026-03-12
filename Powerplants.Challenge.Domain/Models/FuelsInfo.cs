95af9ab7bdb897e20e4e65c007f7410cc6fb00a9
Mirindra HARIVAO <hery.mirindra@outlook.com>
2026-03-13 00:21:19 +0100

    add json properties

--- Powerplants.Challenge.Domain/Models/FuelsInfo.cs
+++ Powerplants.Challenge.Domain/Models/FuelsInfo.cs
@@ -1,7 +1,13 @@
 ﻿using System;
+using System.Text.Json.Serialization;
+
 namespace Powerplants.Challenge.Domain.Models;
 
-public record FuelsInfo(double Gas, double Kerosine, double Co2, double Wind)
+public record FuelsInfo(
+    [property: JsonPropertyName("gas(euro/MWh)")] double Gas,
+    [property: JsonPropertyName("kerosine(euro/MWh)")] double Kerosine,
+    [property: JsonPropertyName("co2(euro/ton)")] double Co2,
+    [property: JsonPropertyName("wind(%)")] double Wind)
 {
 }
 
